a669017be27eb5c7594f28bfeb9926473180ea93
yashpal0502 <cyash0299@gmail.com>
2026-05-20 21:34:35 +0530

    frontend files created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,4 +4,4 @@ const App = () => {
   return <div>App</div>;
 };
 
-export default App;
+export default App; 
---
+++ frontend/src/pages/FlashDeals.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FlashDeals = () => {
+  return (
+    <div>FlashDeals</div>
+  )
+}
+
+export default FlashDeals
