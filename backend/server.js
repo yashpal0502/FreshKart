3bc8ae16fe075cfc62f6ea19eb71086a3cfeb995
yashpal0502 <cyash0299@gmail.com>
2026-07-25 00:48:36 +0530

    initialize Express server

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import "dotenv/config.js";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
