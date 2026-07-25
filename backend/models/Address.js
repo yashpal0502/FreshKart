de902c768172b4b6669201d148eeb864c601d6b5
yashpal0502 <cyash0299@gmail.com>
2026-07-25 23:35:23 +0530

    create address schema

---
+++ backend/models/Address.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const addressSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    pincode: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    isDefault: {
+      type: Boolean,
+      default: false,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timeStamps: true,
+  },
+);
+
+const Address = mongoose.model("Address", addressSchema);
+
+export default Address;
+
+// model Address {
+//   userId    String
+//   label     String
+//   address   String
+//   city      String
+//   state     String
+//   pincode   Number
+//   isDefault Boolean
+//   lat       Float
+//   lng       Float
+//   createdAt DateTime
+//   updatedAt DateTime
+// }
