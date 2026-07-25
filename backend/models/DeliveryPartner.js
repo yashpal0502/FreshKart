7287ebc6f5e9067d696a523952f8347b8abf8bc9
yashpal0502 <cyash0299@gmail.com>
2026-07-26 00:16:34 +0530

    create delivery partner schema

---
+++ backend/models/DeliveryPartner.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const deliveryPartnerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      default: "",
+      required: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    vehicleType: {
+      type: String,
+      default: "bike",
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    orders: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const DeliveryPartner = mongoose.model(
+  "DeliveryPartner",
+  deliveryPartnerSchema,
+);
+
+export default DeliveryPartner;
+
+// model DeliveryPartner {
+//   name        String
+//   email       String
+//   password    String
+//   phone       String
+//   avatar      String?
+//   vehicleType String?
+//   isActive    Boolean?
+//   orders      Order[]
+//   createdAt   DateTime
+//   updatedAt   DateTime
+// }
