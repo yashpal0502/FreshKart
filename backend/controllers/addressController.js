a9f42f43d6c7a76f22e69f1bd791d8218e247bf3
yashpal0502 <cyash0299@gmail.com>
2026-08-12 00:54:55 +0530

    add API to create addresses

--- backend/controllers/addressController.js
+++ backend/controllers/addressController.js
@@ -9,3 +9,49 @@ export const getAddresses = async (req, res) => {
 
   res.json({ addresses });
 };
+
+// Add address
+// POST :- /api/addresses
+export const addAddress = async (req, res) => {
+  const { label, address, city, state, pin, isDefault, lat, lng } = req.body;
+
+  //required coordinates
+  if (lat == null || lng == null) {
+    return res.status(400).json({
+      message:
+        "Location coordinates are required. Please allow location access.",
+    });
+  }
+
+  const currentAddresses = await addressModel.find({ userId: req.user._id });
+
+  let makeDefault = isDefault;
+  if (currentAddresses.length === 0) {
+    makeDefault = true;
+  }
+
+  if (makeDefault) {
+    await addressModel.updateMany(
+      { userId: req.user._id },
+      { $set: { isDefault: false } },
+    );
+  }
+
+  await addressModel.create({
+    userId: req.user._id,
+    label,
+    address,
+    city,
+    state,
+    pincode: pin,
+    isDefault: makeDefault,
+    lat: Number(lat),
+    lng: Number(lng),
+  });
+
+  const addresses = await addressModel
+    .find({ userId: req.user._id })
+    .sort({ createdAt: 1 });
+
+  res.status(201).json({ addresses });
+};
