aae07c937a864bd8e669ba4be7ef29b71335f440
yashpal0502 <cyash0299@gmail.com>
2026-08-13 23:51:19 +0530

    add API to update addresses

--- backend/controllers/addressController.js
+++ backend/controllers/addressController.js
@@ -55,3 +55,83 @@ export const addAddress = async (req, res) => {
 
   res.status(201).json({ addresses });
 };
+
+// Update address
+// PUT :- /api/addresses/:id
+export const updateAddress = async (req, res) => {
+  const { label, address, city, state, pin, isDefault, lat, lng } = req.body;
+
+  // Find address and make sure it belongs to the logged-in user
+  const existingAddress = await addressModel.findOne({
+    _id: req.params._id,
+    userId: req.user._id,
+  });
+
+  if (!existingAddress) {
+    return res.status(404).json({
+      message: "Address not found",
+    });
+  }
+
+  //required coordinates
+  if (lat !== undefined || lng !== undefined) {
+    if (lat == null || lng == null) {
+      return res.status(400).json({
+        message:
+          "Location coordinates are required. Please allow location access.",
+      });
+    }
+  }
+
+  if (isDefault) {
+    await addressModel.updateMany(
+      { userId: req.user._id },
+      { $set: { isDefault: false } },
+    );
+  }
+
+  const data = {};
+
+  if (label) {
+    data.label = label;
+  }
+  if (address) {
+    data.address = address;
+  }
+  if (city) {
+    data.city = city;
+  }
+  if (state) {
+    data.state = state;
+  }
+  if (pin) {
+    data.pin = pin;
+  }
+  if (isDefault !== undefined) {
+    data.isDefault = isDefault;
+  }
+  if (lat != null) {
+    data.lat = Number(lat);
+    data.lng = Number(lng);
+  }
+
+  try {
+    await addressModel.updateOne(
+      {
+        _id: req.params._id,
+        userId: req.user._id,
+      },
+      {
+        $set: data,
+      },
+    );
+  } catch (error) {
+    return res.status(404).json({ message: "Address not found" });
+  }
+
+  const addresses = await addressModel
+    .find({ userId: req.user._id })
+    .sort({ createdAt: 1 });
+
+  res.json({ addresses });
+};
