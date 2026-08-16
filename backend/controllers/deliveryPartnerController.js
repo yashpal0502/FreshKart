295ed8c2c7c47729635d0b78a9a39d688b18063c
yashpal0502 <cyash0299@gmail.com>
2026-08-16 23:02:25 +0530

    add delivery partner deliveries API

--- backend/controllers/deliveryPartnerController.js
+++ backend/controllers/deliveryPartnerController.js
@@ -1,4 +1,5 @@
 import deliveryPartnerModel from "../models/DeliveryPartner.js";
+import orderModel from "../models/Order.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -42,4 +43,25 @@ export const loginPartner = async (req, res) => {
   delete partnerData.password;
 
   res.json({ partner: partnerData, token });
+};
+
+// Get assigned deliveries
+// GET :- /api/delivery/my-deliveries
+export const getMyDeliveries = async (req, res) => {
+  const { status } = req.query;
+
+  const filter = { deliveryPartnerId: req.partner._id };
+
+  if (status === "active") {
+    filter.status = { $in: ["Assigned", "Packed", "Out for Delivery"] };
+  } else if (status === "completed") {
+    filter.status = { $in: ["Delivered", "Cancelled"] };
+  }
+
+  const orders = await orderModel
+    .find(filter)
+    .populate("user", "name email phone")
+    .sort({ createdAt: -1 });
+
+  res.json({ orders });
 };
