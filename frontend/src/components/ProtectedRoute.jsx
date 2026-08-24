8ad5ec14384072bd8b32b876b9e4b42cb350484a
yashpal0502 <cyash0299@gmail.com>
2026-08-25 01:09:14 +0530

    add protected route authentication

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import Loading from "./Loading";
 
 const ProtectedRoute = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
   return <Outlet />;
 };
 
